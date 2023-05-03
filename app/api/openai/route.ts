dd4815275890790efbf8222560f4b254ef32b63b
CAVASOL <awyeon@gmail.com>
2023-05-04 00:22:21 +0800

    mola

---
+++ app/api/openai/route.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+import { Configuration, CreateChatCompletionResponse, OpenAIApi } from "openai";
+import { AxiosResponse } from "axios";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export async function POST(req: Request) {
+  try {
+    const { title, role } = await req.json();
+
+    const aiResponse: AxiosResponse<CreateChatCompletionResponse, any> =
+      await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: `${
+              role || "I am a helpful assistant"
+            }. Write with html tags.`,
+          },
+          {
+            role: "user",
+            content: `Create 3 line blog post with html tags based on this title: ${title}`,
+          },
+        ],
+      });
+
+    return NextResponse.json(
+      {
+        content: aiResponse.data.choices[0].message?.content,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("request error", error);
+    NextResponse.json({ error: "error updating post" }, { status: 500 });
+  }
+}
--- app/post/[id]/Article.tsx
+++ app/post/[id]/Article.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import EditorMenuBar from "./EditorMenuBar";
 import { Editor, EditorContent } from "@tiptap/react";
+import { RocketLaunchIcon } from "@heroicons/react/24/solid";
 
 type Props = {
   contentErr: string;
@@ -17,12 +18,55 @@ const Article = ({
   editor,
   title,
 }: Props) => {
+  const [role, setRole] = useState<string>("I am a helpful assistant.");
+
   if (!editor) {
     return null;
   }
 
+  const postAiContent = async () => {
+    editor
+      .chain()
+      .focus()
+      .setContent("Generating Ai Content. Please Wait...")
+      .run();
+
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/openai`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title: title,
+          role: role,
+        }),
+      }
+    );
+    const data = await response.json();
+
+    editor.chain().focus().setContent(data.content).run();
+    setContent(data.content);
+  };
+
   return (
     <article className="text-wh-500 leading-8">
+      {isEditable && (
+        <div className="border-2 rounded-md bg-wh-50 p-3 mb-3">
+          <h4 className="m-0 p-0">Generate AI Content</h4>
+          <p className="my-1 p-0 text-sm">What type of writer do you want?</p>
+          <div className="flex justify-between gap-5">
+            <input
+              className="border-2 rounded-md bg-wh-50 ps-3 py-1 w-full"
+              placeholder="Role"
+              onChange={(e) => setRole(e.target.value)}
+              value={role}
+            />
+            <button type="button" onClick={postAiContent}>
+              <RocketLaunchIcon className="h-8 w-8 text-accent-orange hover:text-wh-300" />
+            </button>
+          </div>
+        </div>
+      )}
       <div
         className={
           isEditable ? "border-2 rounded-md bg-wh-50 p-3" : "w-full max-w-full"
